24e039c471a0cbb3fbc6208e9e924c21427e5085
SautiereQDev <contact@quentinsautiere.com>
2025-02-09 23:55:33 +0000

    intial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+      {
+        name: "api-pate-mag",
+        script: "./index.ts",
+        watch: true,
+        ignore_watch: ["node_modules", "logs"],
+        // Optionnel : indique le répertoire de travail
+        cwd: "/var/www/api/patate-mag",
+        env: {
+          NODE_ENV: "development",
+        },
+        env_production: {
+          NODE_ENV: "production",
+        },
+      },
+    ],
+  };
---
+++ index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import { connectDB } from './database';
+import articleRoutes from './routes/articles';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT ?? 5000;
+
+const corsOptions = {
+  origin: 'https://quentinsautiere.com', // Allow all origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+
+// Routes
+app.use('/patate-mag/articles', articleRoutes);
+
+// Connect to database and start server
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to connect to the database', error);
+  });
